7316c228050d3070252f1ab971a92f7db4442edb
junhobit <krs199228@gmail.com>
2020-01-17 21:26:05 +0900

    flag.php

---
+++ 2019/Web/Find_flag/flag.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+        <meta charset="utf-8">
+        <script language="javascript" src="flags.js"></script>
+</head>
+<body>
+        <ul id="messages" style="list-style-type:none;padding-left:0px;" >
+        </ul>
+                <input type="text" id="m" onkeydown="if(event.keyCode==13) {linux();clearText(m);}">
+                <input type="button" onclick="linux();clearText(m);" value="send">
+
+</body>
+</html>
